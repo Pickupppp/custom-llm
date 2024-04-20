f0ae2f26f273d074f04c6d33e64b18ee97aa9796
Pickupppp <dingchanghaoo@qq.com>
2024-04-20 23:11:14 +0800

    feat: 完成RotaryEmbedding

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 import torch
 from torch import nn
 
@@ -9,7 +11,7 @@ class CustomRMSNorm(nn.Module):
     RMSNorm 可以看作均值为0的特殊情况。
     """
 
-    def __init__(self, hidden_size, eps=1e-6) -> None:
+    def __init__(self, hidden_size: int, eps: float = 1e-6) -> None:
         super().__init__()
         self.weight = nn.Parameter(torch.ones(hidden_size))
         self.eps = eps
@@ -20,3 +22,57 @@ class CustomRMSNorm(nn.Module):
         variance = hidden_states.pow(2).mean(-1, keepdim=True)
         hidden_states = hidden_states * torch.rsqrt(variance + self.eps)
         return self.weight * hidden_states.to(input_dtype)
+
+
+class CustomRotaryEmbedding(nn.Module):
+    """
+    实现旋转位置编码。
+    """
+
+    def __init__(
+        self,
+        dim,
+        max_position_embeddings: int = 2048,
+        base: int = 10000,
+        device: Union[str, torch.device] = None,
+    ) -> None:
+        super().__init__()
+
+        self.dim = dim
+        self.max_position_embeddings = max_position_embeddings
+        self.base = base
+
+        inv_freq = 1.0 / (
+            self.base ** (torch.arange(0, self.dim, 2).float().to(device) / self.dim)
+        )
+        # 保存固定状态，但不成为模型参数
+        self.register_buffer("inv_freq", inv_freq, persistent=False)
+        self._set_cos_sin_cache(
+            seq_len=self.max_position_embeddings,
+            device=self.inv_freq.device,
+            dtype=torch.get_default_dtype(),
+        )
+
+    def _set_cos_sin_cache(self, seq_len, device, dtype):
+        """
+        设置 cos 和 sin 缓存。
+        """
+        self.max_seq_len_cached = seq_len
+
+        t = torch.arange(
+            self.max_seq_len_cached, device=device, dtype=self.inv_freq.dtype
+        )
+        freqs = torch.outer(t, self.inv_freq)
+        emb = torch.cat((freqs, freqs), dim=-1)
+        # cos_cached / sin_cached 的 shape 为 (seq_len, dim)
+        self.register_buffer("cos_cached", emb.cos().to(dtype), persistent=False)
+        self.register_buffer("sin_cached", emb.sin().to(dtype), persistent=False)
+
+    def forward(self, x: torch.Tensor, seq_len=None):
+        if seq_len > self.max_position_embeddings:
+            self._set_cos_sin_cache(seq_len=seq_len, device=x.device, dtype=x.dtype)
+
+        return (
+            self.cos_cached[:seq_len].to(x.dtype),
+            self.sin_cached[:seq_len].to(x.dtype),
+        )
