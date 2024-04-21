75d740ec3efab6574f2879d9503bc8f90553a6c0
dingchanghao1 <dingchanghao1@jd.com>
2024-04-21 16:18:12 +0800

    feat: 增加 CustomConfig 类

---
+++ config.py
@@ -0,0 +1,25 @@
+class CustomConfig:
+    def __init__(
+        self,
+        vocab_size=151936,
+        hidden_size=4096,
+        intermediate_size=22016,
+        num_hidden_layers=32,
+        num_attention_heads=32,
+        max_position_embeddings=32768,
+        initializer_range=0.02,
+        rms_norm_eps=1e-6,
+        rope_theta=10000.0,
+        attention_dropout=0.0,
+    ) -> None:
+        self.vocab_size = vocab_size
+        self.max_position_embeddings = max_position_embeddings
+        self.hidden_size = hidden_size
+        self.intermediate_size = intermediate_size
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_heads = num_attention_heads
+
+        self.initializer_range = initializer_range
+        self.rms_norm_eps = rms_norm_eps
+        self.rope_theta = rope_theta
+        self.attention_dropout = attention_dropout
