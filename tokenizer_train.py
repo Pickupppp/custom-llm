de8037c74fc276a6a2a2f29d410a8fa7a483c6f7
Pickupppp <dingchanghaoo@qq.com>
2024-04-24 21:41:13 +0800

    feat: 完成 tokenizer 训练

--- tokenizer_train.py
+++ tokenizer_train.py
@@ -1,35 +1,37 @@
-from tokenizers import Tokenizer
-from tokenizers import normalizers, pre_tokenizers
+from tokenizers import Tokenizer, decoders, normalizers, pre_tokenizers, processors
 from tokenizers.models import BPE
-from tokenizers.trainers import BpeTrainer
-from tokenizers.pre_tokenizers import Whitespace, Digits
-from tokenizers.normalizers import NFD, StripAccents, Lowercase
+from tokenizers.normalizers import NFD, Lowercase, StripAccents
+from tokenizers.pre_tokenizers import ByteLevel, Digits, Punctuation, Whitespace
 from tokenizers.processors import TemplateProcessing
-
+from tokenizers.trainers import BpeTrainer
 
 # 初始化 tokenizer
-tokenizer = Tokenizer(BPE(unk_token="<unk>"))
+tokenizer = Tokenizer(BPE())
 
 # 规范化
-normalizer = normalizers.Sequence([NFD(), StripAccents(), Lowercase()])
+normalizer = normalizers.Sequence([NFD(), StripAccents()])
 tokenizer.normalizer = normalizer
 
 # 设置预分词器
-pre_tokenizer = pre_tokenizers.Sequence([Whitespace(), Digits(individual_digits=True)])
+pre_tokenizer = pre_tokenizers.Sequence(
+    [
+        Whitespace(),
+        Punctuation(),
+        Digits(individual_digits=True),
+        ByteLevel(),
+    ]
+)
 tokenizer.pre_tokenizer = pre_tokenizer
 
-# 设置分词模型
-trainer = BpeTrainer(special_tokens=["<s>", "</s>", "<pad>", "<unk>"])
+tokenizer.decoder = decoders.ByteLevel()
+tokenizer.post_processor = processors.ByteLevel()
 
-# # 后处理
-# tokenizer.post_processor = TemplateProcessing(
-#     single="<s> $A </s>",
-#     special_tokens={
-#         "<s>": tokenizer.token_to_id("<s>"),
-#         "</s>": tokenizer.token_to_id("</s>"),
-#         "<pad>": tokenizer.token_to_id("<pad>"),
-#     },
-# )
+
+# 训练
+trainer = BpeTrainer(
+    special_tokens=["<|bos|>", "<|eos|>", "<|system|>", "<|user|>", "<|assistant|>"]
+)
+
 
 # 训练 tokenizer
 tokenizer.train(files=["data/train.csv", "data/test.csv"], trainer=trainer)
